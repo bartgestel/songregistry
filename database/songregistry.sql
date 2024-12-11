7a54c4cb45d92322ee34c2bb2962d4b8c0015cd0
bartgestel <148961278+bartgestel@users.noreply.github.com>
2024-12-11 13:09:44 +0100

    changed sql file

--- database/songregistry.sql
+++ database/songregistry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 27 nov 2024 om 11:20
+-- Gegenereerd op: 11 dec 2024 om 13:08
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.2.12
 
@@ -21,17 +21,6 @@ SET time_zone = "+00:00";
 -- Database: `songregistry`
 --
 
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `albums`
---
-
-CREATE TABLE `albums` (
-  `id` int(11) NOT NULL,
-  `album_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Gegevens worden geëxporteerd voor tabel `albums`
 --
@@ -43,18 +32,6 @@ INSERT INTO `albums` (`id`, `album_name`) VALUES
 (4, 'Blonde'),
 (5, 'Watch The Throne'),
 (6, 'Now, Not Yet');
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `album_artists`
---
-
-CREATE TABLE `album_artists` (
-  `id` int(11) NOT NULL,
-  `album_id` int(11) NOT NULL,
-  `artist_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `album_artists`
@@ -68,17 +45,6 @@ INSERT INTO `album_artists` (`id`, `album_id`, `artist_id`) VALUES
 (5, 5, 2),
 (6, 5, 3),
 (7, 6, 4);
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `artists`
---
-
-CREATE TABLE `artists` (
-  `id` int(11) NOT NULL,
-  `artist_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `artists`
@@ -94,37 +60,12 @@ INSERT INTO `artists` (`id`, `artist_name`) VALUES
 (7, 'Sor'),
 (8, 'Declan McKenna');
 
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `genres`
---
-
-CREATE TABLE `genres` (
-  `id` int(11) NOT NULL,
-  `genre_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Gegevens worden geëxporteerd voor tabel `genres`
 --
 
 INSERT INTO `genres` (`id`, `genre_name`) VALUES
 (1, 'Rap');
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `reviews`
---
-
-CREATE TABLE `reviews` (
-  `id` int(11) NOT NULL,
-  `text` varchar(255) NOT NULL,
-  `rating` double NOT NULL,
-  `song_id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `reviews`
@@ -138,36 +79,12 @@ INSERT INTO `reviews` (`id`, `text`, `rating`, `song_id`, `user_id`) VALUES
 (6, 'Hemels plaatje', 5, 4, NULL),
 (7, 'heerlijk plaatje', 4, 3, NULL);
 
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `roles`
---
-
-CREATE TABLE `roles` (
-  `id` int(11) NOT NULL,
-  `role_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Gegevens worden geëxporteerd voor tabel `roles`
 --
 
 INSERT INTO `roles` (`id`, `role_name`) VALUES
 (1, 'Performer');
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `songs`
---
-
-CREATE TABLE `songs` (
-  `id` int(11) NOT NULL,
-  `song_name` varchar(255) NOT NULL,
-  `song_genre` int(11) NOT NULL,
-  `album_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `songs`
@@ -177,19 +94,6 @@ INSERT INTO `songs` (`id`, `song_name`, `song_genre`, `album_id`) VALUES
 (3, 'IGOR\'S THEME', 1, 1),
 (4, 'EARFQUAKE', 1, 1);
 
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `song_artist`
---
-
-CREATE TABLE `song_artist` (
-  `id` int(11) NOT NULL,
-  `song_id` int(11) NOT NULL,
-  `artist_id` int(11) NOT NULL,
-  `artist_role` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 -- Gegevens worden geëxporteerd voor tabel `song_artist`
 --
@@ -197,177 +101,6 @@ CREATE TABLE `song_artist` (
 INSERT INTO `song_artist` (`id`, `song_id`, `artist_id`, `artist_role`) VALUES
 (5, 3, 1, 1),
 (6, 4, 1, 1);
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `albums`
---
-ALTER TABLE `albums`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `album_artists`
---
-ALTER TABLE `album_artists`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `album_artists_albums_id_fk` (`album_id`),
-  ADD KEY `album_artists_artists_id_fk` (`artist_id`);
-
---
--- Indexen voor tabel `artists`
---
-ALTER TABLE `artists`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `genres`
---
-ALTER TABLE `genres`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `reviews`
---
-ALTER TABLE `reviews`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `reviews_songs_id_fk` (`song_id`),
-  ADD KEY `reviews_users_id_fk` (`user_id`);
-
---
--- Indexen voor tabel `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `songs`
---
-ALTER TABLE `songs`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `songs__genre` (`song_genre`),
-  ADD KEY `songs_albums_id_fk` (`album_id`);
-
---
--- Indexen voor tabel `song_artist`
---
-ALTER TABLE `song_artist`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `artist___id` (`artist_id`),
-  ADD KEY `song___id` (`song_id`),
-  ADD KEY `role___id` (`artist_role`);
-
---
--- Indexen voor tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `albums`
---
-ALTER TABLE `albums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT voor een tabel `album_artists`
---
-ALTER TABLE `album_artists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT voor een tabel `artists`
---
-ALTER TABLE `artists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT voor een tabel `genres`
---
-ALTER TABLE `genres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT voor een tabel `reviews`
---
-ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT voor een tabel `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT voor een tabel `songs`
---
-ALTER TABLE `songs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT voor een tabel `song_artist`
---
-ALTER TABLE `song_artist`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT voor een tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Beperkingen voor geëxporteerde tabellen
---
-
---
--- Beperkingen voor tabel `album_artists`
---
-ALTER TABLE `album_artists`
-  ADD CONSTRAINT `album_artists_albums_id_fk` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`),
-  ADD CONSTRAINT `album_artists_artists_id_fk` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`);
-
---
--- Beperkingen voor tabel `reviews`
---
-ALTER TABLE `reviews`
-  ADD CONSTRAINT `reviews_songs_id_fk` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`),
-  ADD CONSTRAINT `reviews_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Beperkingen voor tabel `songs`
---
-ALTER TABLE `songs`
-  ADD CONSTRAINT `songs__genre` FOREIGN KEY (`song_genre`) REFERENCES `genres` (`id`),
-  ADD CONSTRAINT `songs_albums_id_fk` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`);
-
---
--- Beperkingen voor tabel `song_artist`
---
-ALTER TABLE `song_artist`
-  ADD CONSTRAINT `artist___id` FOREIGN KEY (`artist_id`) REFERENCES `artists` (`id`),
-  ADD CONSTRAINT `role___id` FOREIGN KEY (`artist_role`) REFERENCES `roles` (`id`),
-  ADD CONSTRAINT `song___id` FOREIGN KEY (`song_id`) REFERENCES `songs` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
